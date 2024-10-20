07f1be00c5675511a387faf13ea787db8168ea66
Lars-Erik <80898226+lemoi18@users.noreply.github.com>
2024-10-20 03:09:01 +0200

    Update text_utils.py

--- text_utils.py
+++ text_utils.py
@@ -2,7 +2,8 @@
 import os
 import os.path as osp
 import pandas as pd
-
+import pandas as pd
+import csv
 DEFAULT_DICT_PATH = osp.join('word_index_dict.txt')
 class TextCleaner:
     def __init__(self, word_index_dict_path=DEFAULT_DICT_PATH):
@@ -18,6 +19,6 @@ class TextCleaner:
         return indexes
 
     def load_dictionary(self, path):
-        csv = pd.read_csv(path, header=None).values
+        csv = pd.read_csv(path, header=None,quotechar='"', doublequote=True,escapechar='\\',encoding='utf-8').values
         word_index_dict = {word: index for word, index in csv}
         return word_index_dict
